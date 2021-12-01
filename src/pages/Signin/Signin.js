34d020e80055f85debf5244cb24a06dbcaef6d5f
Fardin Khan <fardinxd10@gmail.com>
2021-12-01 16:17:07 +0530

    Finish signin & signup page

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles \\
 import styles from "./Signin.module.scss";
 
 const Signin = () => {
+  // States \\
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // On Form Submit \\
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log({ email, password });
+  };
+
   // JSX \\
   return (
     <main className="container">
-      <h1>SIGNIN</h1>
+      <h1 className={styles.heading}>SIGNIN</h1>
+
+      <form className={styles.signin_form} onSubmit={submitHandler}>
+        <div className={styles.signin_form_field}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+
+        <div className={styles.signin_form_field}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button>Signin</button>
+      </form>
     </main>
   );
 };
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles \\
 import styles from "./Signup.module.scss";
 
 const Signup = () => {
+  // States \\
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // On Form Submit \\
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log({ name, email, password });
+  };
+
   // JSX \\
   return (
     <main className="container">
-      <h1>SIGNUP</h1>
+      <h1 className={styles.heading}>SIGNUP</h1>
+
+      <form className={styles.signup_form} onSubmit={submitHandler}>
+        <div className={styles.signup_form_field}>
+          <label htmlFor="name">DIsplay Name</label>
+          <input
+            id="name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required
+          />
+        </div>
+
+        <div className={styles.signup_form_field}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+
+        <div className={styles.signup_form_field}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button>Signup</button>
+      </form>
     </main>
   );
 };
