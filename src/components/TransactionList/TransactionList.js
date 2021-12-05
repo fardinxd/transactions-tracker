f2457c55a9434b7f459ba80b7ae9d9ebab1197b1
Fardin Khan <fardinxd10@gmail.com>
2021-12-05 20:35:42 +0530

    Create TransactionList component

---
+++ src/components/TransactionList/TransactionList.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+// Styles & Icons \\
+import styles from "./TransactionList.module.scss";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
+// Custom Hooks \\
+import { useFirestore } from "../../hooks/useFirestore";
+
+const TransactionList = ({ transactions }) => {
+  // Delete Transaction With useFirestore Hook \\
+  const { deleteDocument } = useFirestore("transactions");
+
+  // Date & Time \\
+  const dateAndTime = (seconds) => {
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let date = new Date(seconds * 1000).getDate(),
+      monthIndex = new Date(seconds * 1000).getMonth(),
+      year = new Date(seconds * 1000).getFullYear(),
+      hours = new Date(seconds * 1000).getHours(),
+      minutes = new Date(seconds * 1000).getMinutes();
+
+    if (String(minutes).length === 1) minutes = `0${minutes}`;
+    if (String(hours).length === 1) hours = `0${hours}`;
+
+    return (
+      <React.Fragment>
+        <span>
+          {date}-{monthNames[monthIndex]}-{year}
+        </span>
+
+        <span>
+          {hours}:{minutes}
+        </span>
+      </React.Fragment>
+    );
+  };
+
+  // Number To Currency Formatter \\
+  const numberToCurrency = (number) => {
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    return formatter.format(Number(number));
+  };
+
+  // JSX \\
+  return (
+    <React.Fragment>
+      <h1>
+        {transactions.length === 0
+          ? "You Don't Have Any Transactions"
+          : "Your Transactions"}
+      </h1>
+
+      <ul className={styles.transactions}>
+        {transactions.map((transaction) => (
+          <li className={styles.transactions_item} key={transaction.id}>
+            <div className={styles.transactions_item_info}>
+              <h2>{transaction.name}</h2>
+              <h3>{numberToCurrency(transaction.amount)}</h3>
+            </div>
+
+            <div className={styles.transactions_item_date}>
+              {dateAndTime(transaction.createdAt.seconds)}
+            </div>
+
+            <div
+              className={styles.transactions_item_delete}
+              onClick={() => deleteDocument(transaction.id)}
+            >
+              <RiDeleteBin5Line />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default TransactionList;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,18 +4,29 @@ import React from "react";
 import styles from "./Home.module.scss";
 
 // Components \\
+import TransactionList from "../../components/TransactionList/TransactionList";
 import TransactionForm from "../../components/TransactionForm/TransactionForm";
 
 // Custom Hooks \\
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 const Home = () => {
   // Auth Context \\
   const { currentUser } = useAuthContext();
 
+  // Getting 'Transactions' Collection Form Firebase Firestore Using useCollection Hook \\
+  const { documents, error } = useCollection(
+    "transactions",
+    ["uid", "==", currentUser.uid],
+    ["createdAt", "desc"]
+  );
+
   // JSX \\
   return (
     <main className={`container ${styles.home}`}>
+      {documents && !error && <TransactionList transactions={documents} />}
+      {error && <div className="error">{error}</div>}
       <TransactionForm uid={currentUser.uid} />
     </main>
   );
